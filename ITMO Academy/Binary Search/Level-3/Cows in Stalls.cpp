d4991d8b5738ca18f64a93280a0b23674e9c94be
AG1911765 <arpit.goyal@cerner.com>
2024-03-25 22:49:55 +0530

    Adds solution for Cows in Stalls (ITMO, Minimax, Binary Search)

--- ITMO Academy/Binary Search/Level-3/Cows in Stalls.cpp
+++ ITMO Academy/Binary Search/Level-3/Cows in Stalls.cpp
@@ -2,11 +2,38 @@
  
 #include <iostream>
 #include <vector>
+#include <algorithm>
  
 using namespace std;
  
+// M = 1e9
+// Time: O(NlogNlogM)
+// Space: O(1)
+
+bool can_arrange(int m, vector<int>& stalls, int N, int K) {
+    int curr = 0;
+    for (int i = 1; i < K; i++) {
+        int next = stalls[curr] + m;
+        auto itr = lower_bound(stalls.begin(), stalls.end(), next);
+        if (itr == stalls.end())
+            return false;
+
+        curr = itr-stalls.begin();
+    }
+
+    return true;
+}
+
 void solve(int N, vector<int>& stalls, int K) {
- 
+    int l = 1, h = 1e9;
+    while (l <= h) {
+        int m = (h-l)/2 + l;
+        if (can_arrange(m, stalls, N, K))
+            l = m+1;
+        else h = m-1;
+    }
+
+    cout << h << endl;
 }
  
 int main() {

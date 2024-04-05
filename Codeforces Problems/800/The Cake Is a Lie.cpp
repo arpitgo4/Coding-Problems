727a2677f05eeaf226e61317bef3d31db2a8f65d
AG1911765 <arpit.goyal@cerner.com>
2024-04-05 17:07:35 +0530

    Adds updated solution for problem,
    * The Cake is a lie (Maths)

--- Codeforces Problems/800/The Cake Is a Lie.cpp
+++ Codeforces Problems/800/The Cake Is a Lie.cpp
@@ -1,40 +1,14 @@
 // The Cake Is a Lie (https://codeforces.com/problemset/problem/1519/B)
  
 #include <iostream>
-#include <vector>
-#include <bitset>
  
 using namespace std;
  
-// Time: O(N * M * K)
-// Space: O(N * M * K)
-
-// 1e8 ~ 100 * 1e6 MB (this is in bytes)
-// 4 * 100MB = 400 MB (due to using integer) (memory limit is 256MB)
-
-#define bit bitset<1>
-
-vector<vector<vector<bit>>> dp;
-
-bit dfs(int i, int j, int k, int N, int M) {
-    if (i < 0 || i == N || j < 0 || j == M || k < 0)
-        return 0;
-    if (i == N-1 && j == M-1)
-        return k == 0 ? 1 : 0;
-    if (dp[i][j][k] != -1)
-        return dp[i][j][k];
-
-    bit c1 = dfs(i+1, j, k-(j+1), N, M);
-    bit c2 = dfs(i, j+1, k-(i+1), N, M);
-
-    return dp[i][j][k] = c1 | c2;
-}
+// Time: O(1)
+// Space: O(1)
 
 bool solve(int N, int M, int K) {
-    dp.assign(N, vector<vector<bit>>(M, vector<bit>(K+1, -1)));
-
-    bit possible = dfs(0, 0, K, N, M);
-    return possible.count() == 1;
+    return (N * M - 1) == K;
 }
  
 int main() {

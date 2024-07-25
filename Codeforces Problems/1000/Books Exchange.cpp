6f19a113e2a667f70e693d5792b7dbf0ef0a1892
Arpit Goyal <arpit.go4@gmail.com>
2024-07-26 01:32:08 +0530

    Adds solution to problem,
    * Books Exchange (Trees, Implementation)

--- Codeforces Problems/1000/Books Exchange.cpp
+++ Codeforces Problems/1000/Books Exchange.cpp
@@ -5,36 +5,64 @@
  
 using namespace std;
  
-// Time: O()
-// Space: O()
+// Time: O(N)
+// Space: O(N)
  
-vector<int> P, S;
- 
-void solve(vector<int>& A, int N) {
-    P.assign(N+1, -1);
-    S.assign(N+1, 1);
-    for (int i = 0; i < N+1; i++)
-        P[i] = i;
+/**
+ * find all cycles length
+ * 
+ * for any node, whatever is the length of cycle is
+ * that is the answer for it.
+ * 
+ * for node's pointing to themselves, answer is 1.
+ * i.e. number of edges in the cycle.
+ * 
+ * Thought: In iteration, we have to take decision or solve the problem
+ *          then and there.
+ *          In recursion, we are free to take decision or solve the problem
+ *          after exploring all the subproblems and having a wider look at
+ *          all the problem space.
+ */
 
-    
+vector<int> vis, len;
+
+int dfs(int u, int depth, vector<int>& P) {
+    if (vis[u] == 1)
+        return depth;
+
+    vis[u] = 1;
+    int v = P[u];
+    return len[u] = dfs(v, depth+1, P);
+}
+
+void solve(vector<int>& P, int N) {
+    vis.assign(N+1, 0);
+    len.assign(N+1, 0);
+
+    for (int i = 1; i <= N; i++)
+        dfs(i, 0, P);
+
+    for (int i = 1; i <= N; i++)
+        cout << len[i] << " ";
+    cout << endl;
 }
  
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int Q;
-    cin >> Q;
+    int T;
+    cin >> T;
 
-    while (Q--) {
+    while (T--) {
         int N;
         cin >> N;
 
-        vector<int> A(N);
-        for (int i = 0; i < N; i++)
-            cin >> A[i];
+        vector<int> P(N+1);
+        for (int i = 1; i <= N; i++)
+            cin >> P[i];
 
-        solve(A, N);
+        solve(P, N);
     }
     
     return 0;

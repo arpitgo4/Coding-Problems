f375bc69ff8c6ff8e042651fc478ff9fdb8033de
Arpit Goyal <arpit.go4@gmail.com>
2025-05-12 16:04:18 +0530

    Adds solution to problem,
    * Hello (Introductory Problem)

---
+++ Online Judge Problems/Competitive Programming 4/Introduction/IO + Sequences Only/hello.cpp
@@ -0,0 +1,17 @@
+// hello (https://open.kattis.com/problems/hello)
+ 
+#include <iostream>
+ 
+using namespace std;
+ 
+// Time: O(1)
+// Space: O(1)
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cout << "Hello World!" << endl;
+    
+    return 0;
+}
